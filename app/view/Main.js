cccc9ec6e478e0489597a8fbb6d6732681251e2b
Constantin Lebrecht <constantin@monavari.de>
2012-12-27 19:33:15 +0100

    add some simple Driver Log UI elements

--- app/view/Main.js
+++ app/view/Main.js
@@ -3,36 +3,42 @@
 
   Ext.define("DiversLog.view.Main", {
     extend: "Ext.tab.Panel",
-    xtype: "main",
-    requires: ["Ext.TitleBar", "Ext.Video"],
+    alias: "widget.mainView",
+    requires: ["Ext.TitleBar"],
+    onAddLogEntryTap: function() {
+      return console.log("test");
+    },
     config: {
       tabBarPosition: "bottom",
       items: [
         {
-          title: "Welcome",
-          iconCls: "home",
-          styleHtmlContent: true,
+          title: "Driver's Log",
+          iconCls: "speedometer",
           scrollable: true,
-          items: {
-            docked: "top",
-            xtype: "titlebar",
-            title: "Welcome to Sencha Touch 2"
-          },
-          html: ["You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ", "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ", "and refresh to change what's rendered here."].join("")
-        }, {
-          title: "Get Started",
-          iconCls: "action",
           items: [
             {
               docked: "top",
               xtype: "titlebar",
-              title: "Getting Started"
-            }, {
-              xtype: "video",
-              url: "http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c",
-              posterUrl: "http://b.vimeocdn.com/ts/261/062/261062119_640.jpg"
+              title: "Driver's Log",
+              items: [
+                {
+                  xtype: "button",
+                  ui: "action",
+                  iconCls: "add",
+                  align: "right",
+                  iconMask: true,
+                  itemId: "addNoteButton"
+                }
+              ]
             }
           ]
+        }
+      ],
+      listeners: [
+        {
+          delegate: "#addNoteButton",
+          event: "tap",
+          fn: "onAddLogEntryTap"
         }
       ]
     }
